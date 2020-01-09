51a9213a628e92687318b1940cf7922201ade8f9
DenisVolkov12 <50923209+DenisVolkov12@users.noreply.github.com>
2020-01-10 01:47:36 +0300

    Update Leap.java
    была ошибка и стоял знак "минус". т.к. каждый 400-й год считается високосным, нужно использовать знак сложения)

--- csc-java/week_02/leap_year/Leap.java
+++ csc-java/week_02/leap_year/Leap.java
@@ -12,7 +12,7 @@
 
 public class Leap {
     public static int leapYearCount(int year) {
-        return (year - (year % 4)) / 4 - ((year - (year % 100)) / 100 - (year - (year % 400)) / 400);
+        return (year - (year % 4)) / 4 - ((year - (year % 100)) / 100 + (year - (year % 400)) / 400);
     }
 }
 
